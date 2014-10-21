b3c9d8ce6a73bb17f0b181cbdfe6119a4f9fd399
Brian Jones <drf0124@yahoo.com>
2014-10-21 19:02:45 -0400

    added the 1st product; and styled some

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+Product.delete_all 
+
+Product.create!(title: 'Average Band',
+	description: %{<p>
+					 The Average Band gives you 25 pounds of resistance 
+					 </p>},
+	image_url: 'greenband.jpg',
+	price: 27.99)
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
